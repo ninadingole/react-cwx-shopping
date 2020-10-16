df1d27764d50cf9bc81a3303cc53f90ba21b388b
Neenad Ingole <neenad@shippit.com>
2020-10-16 13:39:36 +0530

    add cart hidding feature using redux

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 import './header.styles.scss';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import CartIcon from '../cart-icon/cart-icon.component';
 
-const Header = () => (
+const Header = ({ hidden }) => (
     <div className='header'>
         <Link to='/' className='logo-container'>
             <Logo className='logo' />
@@ -15,7 +17,13 @@ const Header = () => (
             <Link className='option' to='/contact'>CONTACT</Link>
             <CartIcon />
         </div>
+        { hidden ? null : <CartDropdown />}
     </div>
 );
 
-export default Header;
+
+const mapStateToProps = ({cart: { hidden }}) => ({
+    hidden,
+})
+
+export default connect(mapStateToProps)(Header);
---
+++ src/redux/root-reducer.js
@@ -0,0 +1,4 @@
+import { combineReducers } from 'redux';
+import cartReducer from './cart/cart.reducer';
+
+export default combineReducers({ cart: cartReducer });
